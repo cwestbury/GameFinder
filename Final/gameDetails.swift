c67cdc0664facf50a7be7a6384b980e266ed6dd4
Cameron Westbury <cameron.westbury@gmail.com>
2015-11-30 17:37:39 -0500

    end of day 1

---
+++ Final/gameDetails.swift
@@ -0,0 +1,13 @@
+//
+//  gameDetails.swift
+//  Final
+//
+//  Created by Cameron Westbury on 11/30/15.
+//  Copyright © 2015 Cameron Westbury. All rights reserved.
+//
+
+import Cocoa
+
+class gameDetails: UIViewController {
+
+}
