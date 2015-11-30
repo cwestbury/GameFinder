21b3392f0f62b8033c2ae39b2e9cd3f64e1463a6
Cameron Westbury <cameron.westbury@gmail.com>
2015-11-30 09:43:42 -0500

    Added Parse and Parse UI

---
+++ Podfile
@@ -0,0 +1,11 @@
+platform :ios, '8.0'
+use_frameworks!
+
+target 'Final' do
+
+pod 'Parse', '~> 1.9'
+pod 'ParseUI', '~> 1.1'
+
+
+end
+
---
+++ Pods/Target Support Files/Pods-Final/Pods-Final-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_FinalVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_FinalVersionString[];
+
