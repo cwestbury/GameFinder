73987f54717483e3cac85f21b453b69eaf490bd7
Cameron Westbury <cameron.westbury@gmail.com>
2015-12-15 15:42:55 -0500

    login working better

---
+++ Final/CustomMapTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomMapTableViewCell.swift
+//  Final
+//
+//  Created by Cameron Westbury on 12/14/15.
+//  Copyright © 2015 Cameron Westbury. All rights reserved.
+//
+
+import UIKit
+
+class CustomMapTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
